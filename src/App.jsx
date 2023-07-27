99662c68c5161a5ea720e4e2e92c8812839ae439
jashan <jashannoor2001@gmail.com>
2023-07-26 23:50:04 -0600

    Bug Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,56 @@
-import React , {Component, useEffect} from 'react';
+import React, { Component, useEffect } from "react";
 
-import CSS from './App.css'
+import CSS from "./App.css";
 
 // importing routes for navigation
 import { BrowserRouter, Routes, Route, Link, json } from "react-router-dom";
 
-import dummyNotes from './dummy-notes'
+import dummyNotes from "./dummy-notes";
 
 //import useState
 
-import {useState} from "react"
-
+import { useState } from "react";
 
 //import pages
 import Notes from "./pages/Notes";
 import CreateNote from "./pages/CreateNote";
-import EditNote from "./pages/EditNote"
+import EditNote from "./pages/EditNote";
 
 function App() {
   //if local storage has no notes take it as an empty string to render the view
-  const[notes, setNotes]=useState( JSON.parse(localStorage.getItem('notes')) || [])
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem("notes")) || []
+  );
 
-  useEffect(()=>{
-    localStorage.setItem('notes',JSON.stringify(notes))
-  })
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  });
 
-//   useEffect(() => {
-//     const handleContextmenu = e => {
-//         e.preventDefault()
-//     }
-//     document.addEventListener('contextmenu', handleContextmenu)
-//     return function cleanup() {
-//         document.removeEventListener('contextmenu', handleContextmenu)
-//     }
-// }, [ ])
+  useEffect(() => {
+    const handleContextmenu = (e) => {
+      e.preventDefault();
+    };
+    document.addEventListener("contextmenu", handleContextmenu);
+    return function cleanup() {
+      document.removeEventListener("contextmenu", handleContextmenu);
+    };
+  }, []);
 
   return (
     <div className="container">
-    
-    
-
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Notes notes={notes}/>} />
-            <Route path="/create-note" element={<CreateNote setNotes={setNotes}/>} />
-            <Route path='/edit-note/:id' element={<EditNote notes={notes} setNotes={setNotes}/>}/>
-          </Routes>
-        </BrowserRouter>
-      
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Notes notes={notes} />} />
+          <Route
+            path="/create-note"
+            element={<CreateNote setNotes={setNotes} />}
+          />
+          <Route
+            path="/edit-note/:id"
+            element={<EditNote notes={notes} setNotes={setNotes} />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
